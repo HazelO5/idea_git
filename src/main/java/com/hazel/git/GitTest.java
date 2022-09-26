30a7cea7fe0944dad2d26d0f868d1294289981a8
Hazel <hazelonew@gamil.com>
2022-09-26 09:11:19 +0800

    create files

---
+++ src/main/java/com/hazel/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hazel.git;
+
+/**
+ * @author Hazel
+ * @date 2022/9/26 9:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git --> create files");
+    }
+}
