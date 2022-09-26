b9cc61931255cc030716d6d034b56be3759f897d
Hazel <hazelonew@gamil.com>
2022-09-26 09:14:06 +0800

    first update

--- src/main/java/com/hazel/git/GitTest.java
+++ src/main/java/com/hazel/git/GitTest.java
@@ -7,5 +7,6 @@ package com.hazel.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git --> create files");
+        System.out.println("Git --> first update");
     }
 }
